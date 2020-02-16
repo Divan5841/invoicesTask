f9891734f1cb8cd3285f701a58ab07a64c61c3e8
Divan5841 <60582147+Divan5841@users.noreply.github.com>
2020-02-16 21:19:30 +0300

    Update getInvoice.js

--- getInvoice.js
+++ getInvoice.js
@@ -1,69 +1,89 @@
+const order = {
+    "customer": "MDT",
+    "performance": [
+        {
+            "playId": "Гамлет",
+            "audience": 55,
+            "type": "tragedy"
+        },
+        {
+            "playId": "Ромео и Джульетта",
+            "audience": 35,
+            "type": "tragedy"
+        },
+        {
+            "playId": "Отелло",
+            "audience": 40,
+            "type": "comedy"
+        }
+    ]
+};
+
+
 const PERFORMANCE_TYPE = {
-  TRAGEDY: "tragedy",
-  COMEDY: "comedy"
+    TRAGEDY: "tragedy",
+    COMEDY: "comedy"
 };
 
 const PERFORMANCE_PRICE = {
-  TRAGEDY: 40000,
-  COMEDY: 30000
+    TRAGEDY: 40000,
+    COMEDY: 30000
 };
 
 const getPrice = (type, audience) => {
-  if (type === PERFORMANCE_TYPE.TRAGEDY) {
-    if (audience > 30) {
-      return PERFORMANCE_PRICE.TRAGEDY + 1000 * (audience - 30);
+    if (type === PERFORMANCE_TYPE.TRAGEDY) {
+        if (audience > 30) {
+            return PERFORMANCE_PRICE.TRAGEDY + 1000 * (audience - 30);
+        }
+        return PERFORMANCE_PRICE.TRAGEDY;
     }
-    return PERFORMANCE_PRICE.TRAGEDY;
-  }
-  if (type === PERFORMANCE_TYPE.COMEDY) {
-    if (audience > 20) {
-      return PERFORMANCE_PRICE.COMEDY + 16000 + 800 * (audience - 20);
+    if (type === PERFORMANCE_TYPE.COMEDY) {
+        if (audience > 20) {
+            return PERFORMANCE_PRICE.COMEDY + 16000 + 800 * (audience - 20);
+        }
+        return PERFORMANCE_PRICE.COMEDY + audience * 300;
     }
-    return PERFORMANCE_PRICE.COMEDY + audience * 300;
-  }
-  throw new Error(`Неизвестный тип: ${type}`);
+    throw new Error(`Неизвестный тип: ${type}`);
 };
 
 const getInvoice = order => {
-  let totalPrice = 0;
-  let totalBonus = 0;
-  let comedyBonus = 0;
-  let counterComedy = 0;
+    let totalPrice = 0;
+    let totalBonus = 0;
+    let comedyBonus = 0;
+    let counterComedy = 0;
 
-  const numberFormat = new Intl.NumberFormat("ru-RU", {
-    style: "currency",
-    currency: "RUB",
-    minimumFractionDigits: 2
-  });
+    const numberFormat = new Intl.NumberFormat("ru-RU", {
+        style: "currency",
+        currency: "RUB",
+        minimumFractionDigits: 2
+    });
 
-  let result = `Счет для ${order.customer}\n`;
+    let result = `Счет для ${order.customer}\n`;
 
-  order.performance.forEach(({ type, audience, playId }) => {
-    const currentPrice = getPrice(type, audience);
+    order.performance.forEach(({type, audience, playId}) => {
+        const currentPrice = getPrice(type, audience);
 
-    result = `${result}${playId}: ${numberFormat.format(
-      currentPrice
-    )} (${audience} мест)\n`;
-    totalPrice += currentPrice;
-    totalBonus += Math.max(audience - 30, 0);
+        result = `${result}${playId}: ${numberFormat.format(
+            currentPrice
+        )} (${audience} мест)\n`;
+        totalPrice += currentPrice;
+        totalBonus += Math.max(audience - 30, 0);
 
-    if (type === PERFORMANCE_TYPE.COMEDY) {
-      comedyBonus += Math.floor(audience / 5);
-      counterComedy++;
-    }
+        if (type === PERFORMANCE_TYPE.COMEDY) {
+            comedyBonus += Math.floor(audience / 5);
+            counterComedy++;
+        }
 
-    if (counterComedy === 10) {
-      totalBonus += comedyBonus;
-      comedyBonus = 0;
-      counterComedy = 0;
-    }
-  });
+        if (counterComedy === 10) {
+            totalBonus += comedyBonus;
+            comedyBonus = 0;
+            counterComedy = 0;
+        }
+    });
 
-  return `${result}Итого с вас: ${numberFormat.format(
-    totalPrice
-  )}\nВы заработали ${totalBonus} бонусов\n`;
+    return `${result}Итого с вас: ${numberFormat.format(
+        totalPrice
+    )}\nВы заработали ${totalBonus} бонусов\n`;
 };
 
-fetch("./order.json")
-  .then(response => response.json())
-  .then(data => console.log(getInvoice(data)));
+console.log(getInvoice((order)));
